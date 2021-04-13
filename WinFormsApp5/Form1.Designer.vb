6c36e206dbd187f0ca61431c4dad3d685f919f7a
Adam-Crook <82472158+Adam-Crook@users.noreply.github.com>
2021-04-13 21:21:39 +0100

    Add files via upload

---
+++ WinFormsApp5/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CustomerDataGridView = New System.Windows.Forms.DataGridView()
+        CType(Me.CustomerDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CustomerDataGridView
+        '
+        Me.CustomerDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.CustomerDataGridView.Location = New System.Drawing.Point(12, 12)
+        Me.CustomerDataGridView.Name = "CustomerDataGridView"
+        Me.CustomerDataGridView.Size = New System.Drawing.Size(776, 378)
+        Me.CustomerDataGridView.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.CustomerDataGridView)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.CustomerDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CustomerDataGridView As DataGridView
+End Class
---
+++ WinFormsApp5/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Configuration
+Imports System.Data
+Imports System.Data.SqlDbType
+Imports System.Configuration.ConfigurationErrorsException
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    'Below is where I initialize the finding of the customer list'
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CustomerDataGridView.DataSource = findcustomerlist()
+
+    End Sub
+
+    Private Function findcustomerlist() As DataTable
+        'Defines the datatablecustomerlist as a datatable data type'
+        Dim datatablecustomerlist As New DataTable
+        'Below is where I define the connection to the database, this line of code causes a problem and I am not sure how to fix
+        Dim connstring As String = ConfigurationManager.ConnectionStrings("custpull").ConnectionString
+
+        'connection to db'
+        Using conn As New SqlConnection(connstring)
+
+            Using cmd As New SqlCommand("SELECT * FROM dbo.customers", conn)
+
+                ' open the connection'
+                conn.Open()
+                Dim reader As SqlDataReader = cmd.ExecuteReader()
+
+                datatablecustomerlist.Load(reader)
+            End Using
+        End Using
+        'Return the database list to the DGV'
+        Return datatablecustomerlist
+    End Function
+
+End Class
